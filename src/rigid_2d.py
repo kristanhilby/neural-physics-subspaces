96d7a0745fddd8f481cf86001ee408b4ad775a89
kristanhilby <kmhilby@mit.edu>
2023-07-07 17:50:28 -0400

    Update rigid_2d.py

--- src/rigid_2d.py
+++ src/rigid_2d.py
@@ -5,107 +5,110 @@ import os
 import polyscope as ps
 import polyscope.imgui as psim
 
+try:
+    import igl
+finally:
+    print("WARNING: igl bindings not available")
+
 import utils
 import config
+
+# Define general functions for defining and making bodies
+def make_body(file, density, scale):
+
+    v, f = igl.read_triangle_mesh(file)
+    v = scale*v
+
+    vol = igl.massmatrix(v,f).data
+    vol = np.nan_to_num(vol) # massmatrix returns Nans in some stewart meshes
+
+    c = np.sum( vol[:,None]*v, axis=0 ) / np.sum(vol) 
+    v = v - c
+
+    W = np.c_[v, np.ones(v.shape[0])]
+    mass = np.matmul(W.T, vol[:,None]*W) * density
+
+    x0 = jnp.array( [c[0], 0, c[1], 0, c[2], 0, 0, 0, 0, 0, 0, 0] )
+
+    body = {'v': v, 'f': f, 'W':W, 'x0': x0, 'mass': mass }
+    return body
+
+def make_joint( b0, b1, bodies, joint_pos_world, joint_vec_world ):
+    # Creates a joint between the specified bodies, assumes the bodies have zero rotation and are properly aligned in the world
+    # TODO: Use rotation for joint initialization
+    pb0 = joint_pos_world
+    vb0 = joint_vec_world
+    if b0 != -1:
+        c0 = bodies[b0]['x0'][3,:]
+        pb0 = pb0 - c0
+    pb1 = joint_pos_world
+    vb1 = joint_vec_world
+    if b1 != -1:
+        c1 = bodies[b1]['x0'][3,:]
+        pb1 = pb1 - c1
+    joint = {'body_id0': b0, 'body_id1': b1, 'pos_body0': pb0, 'pos_body1': pb1, 'vec_body0': vb0, 'vec_body1': vb1}
+    return joint
+
+def bodiesToStructOfArrays(bodies):
+    v_arr = []
+    f_arr = []
+    W_arr = []
+    x0_arr = []
+    mass_arr = []
+    for b in bodies:
+        v_arr.append(b['v'])
+        f_arr.append(b['f'])
+        W_arr.append(b['W'])
+        x0_arr.append(b['x0'])
+        mass_arr.append(b['mass'])
+    
+    out_struct = {
+        'v'     : jnp.stack(v_arr, axis=0),
+        'f'     : jnp.stack(f_arr, axis=0),
+        'W'     : jnp.stack(W_arr, axis=0),
+        'x0'    : jnp.stack(x0_arr, axis=0),
+        'mass'  : jnp.stack(mass_arr, axis=0),
+    }
+
+    n_bodies = len(v_arr)
+
+    return out_struct, n_bodies
 
 class rigid2d:
 
     @staticmethod
     def construct(problem_name):
-
-        '''
-
-        Basic philosophy:
-            We define the system via two objects, a object instance ('system'), which can
-            hold pretty much anything (strings, function pointers, etc), and a dictionary 
-            ('system_def') which holds only jnp.array objects.
-
-            The reason for this split is the JAX JIT system. Data stored in the `system`
-            is fixed after construction, so it can be anything. Data stored in `system_def`
-            can potentially be modified after the system is constructed, so it must consist
-            only of JAX arrays to make JAX's JIT engine happy.
-
-
-        The fields which MUST be populated are:
-       
-            = System name
-            system.system_name --> str
-            The name of the system (e.g. "neohookean")
-            
-
-            = Problem name
-            system.problem_name --> str
-            The name of the problem (e.g. "trussbar2")
-       
-
-            = Dimension
-            system.dim --> int
-                The dimension of the configuration space for the system. When we
-                learn subspaces that map from `R^d --> R^n`, this is `n`. If the 
-                system internally expands the configuration space e.g. to append 
-                additional pinned vertex positions, those should NOT be counted 
-                here.
-
-
-            = Initial position
-            system_def['init_pos'] --> jnp.array float dimension: (n,)
-                An initial position, used to set the initial state in the GUI
-                It does not necessarily need to be the rest pose of the system.
-            
-
-            = Conditional parameters dimension
-            system.cond_dim --> int
-                The dimension of the conditional parameter space for the system. 
-                If there are no conditional parameters, use 0.
-            
-            = Conditional parameters value
-            system_def['cond_param'] --> jnp.array float dimension: (c,)
-                A vector of conditional paramters for the system, defining its current
-                state. If there are no conditional parameters, use a length-0 vector.
-            
-
-            = External forces
-            system_def['external_forces'] --> dictionary of anything
-                Data defining external forces which can be adjusted at runtime
-                The meaning of this dictionary is totally system-dependent. If unused, 
-                leave as an empty dictionary. Fill the dictionary with arrays or other 
-                data needed to evaluate external forces.
-
-
-            = Interesting states
-            system_def['interesting_states'] --> jnp.array float dimension (i,n)
-                A collection of `i` configuration state vectors which we want to explicitly track
-                and preserve.
-                If there are no interesting states (i=0), then this should just be a size (0,n) array.
-
-
-            The dictionary may also be populated with any other user-values which are useful in 
-            defining the system.
-
-            NOTE: When you add a new system class, also add it to the registry in config.py 
-        '''
-
         system_def = {}
         system=rigid2d()
-
 
         # Example values:
         # (dummy data)
         system_def['system_name'] = "rigid2d"
-        system_def['problem_name'] = problem_name
-        system_def['cond_params'] = jnp.zeros((0,)) # a length-0 array
+        system.problem_name = str(problem_name)
+        
+        # Set some default parameters
         system_def['external_forces'] = {}
+        system_def['cond_param'] = jnp.zeros((0,))
+        system_def["contact_stiffness"] = 1000000.0
+        system.cond_dim = 0
+        system.body_ID = None
 
         bodies = []
         joint_list = []
         numBodiesFixed = 0
 
         if problem_name == 'simple_pendulum':
-            config_dim = 2
+            # Declate number of fixed bodies
+            numBodiesFixed = 0
+
+            # Include the relevant bodies
+            bodies.append( make_body( os.path.join(".", "data", "simplependulum_pendulum.obj"), 1000, 1.0) )
             
-            system_def['mass'] = 5
-            system_def['length'] = 10
-            system_def['dim'] = config_dim
+            # Create the joints between the bodies
+            joint_list.append( make_joint( 0, -1, bodies, jnp.array([ 12.5, 0.0 ,12.5 ]), jnp.array([ 0, 1.0, 0.0 ]) ) )
+            
+            system_def['length'] = 50
+
             system_def['init_pos'] = jnp.zeros(config_dim) # some values
             system_def['interesting_states'] = jnp.zeros((0,config_dim))
             system_def['x0'] = jnp.array([0, 0])
@@ -116,7 +119,6 @@ class rigid2d:
             system_def['gravity'] = jnp.array([0.0, 0.0, -9.8])
         
         elif problem_name == 'problem_B':
-
             # and so on....
             config_dim = 334
             system_def['dim'] = config_dim
@@ -126,13 +128,30 @@ class rigid2d:
         else:
             raise ValueError("could not parse problem name: " + str(problem_name))
 
-        pos  = system_def['x0']
+        posFixed  = jnp.array( np.array([ body['x0']   for body in bodies[0:numBodiesFixed] ]).flatten() )
+        pos  = jnp.array( np.array([ body['x0']   for body in bodies[numBodiesFixed:] ]).flatten() )
+
+        mass = jnp.array( np.array([ body['mass'] for body in bodies[numBodiesFixed:] ]).flatten() )
+        
+        #
         system.dim = pos.size
-        system.cond_dim = 0
+
+        system.bodiesRen = bodies
+        system.n_bodies = len(bodies)
+        
+        #
+        system.joints = joint_list
+
+        system_def['fixed_pos'] = posFixed
+        system_def['rest_pos'] = pos
+        system_def['init_pos'] = pos
+        system_def['mass'] = mass
+        system_def['dim'] = pos.size
+
         system_def['interesting_states'] = system_def['init_pos'][None,:]
 
-
         return system, system_def
+
   
     # ===========================================
     # === Energy functions 
@@ -141,33 +160,77 @@ class rigid2d:
     # These define the core physics of our system
 
     def potential_energy(self, system_def, q):
-        qR = q.reshape(-1,2,1)
-        q_pos = jnp.take(qR, jnp.array([0]))
-        lengthR = system_def['length']
+        # Joint contact energy
+        qRFull = jnp.concatenate((system_def['fixed_pos'],q)).reshape(-1,12,1)
+        qr_linpos = jnp.array([qRFull[:,0,1], qRFull[:,2,1], qRFull[:,4,1]])
 
-        height = lengthR * jnp.cos(q_pos)
-        massR = system_def['mass']
-        gravity = system_def['gravity']  
+        ###########
 
-        gravity_energy = height * massR * gravity[None,2]
+        joint_energy = 0.0
 
-        return gravity_energy
+        for j in self.joints:
+
+            pb0 = j['pos_body0']
+            vb0 = j['vec_body0']
+            
+            b0id = j['body_id0']
+            if b0id != -1:
+                # transform point on body to point in world
+                pb0 = jnp.matmul(jnp.append(pb0,1), qr_linpos[b0id])
+                vb0 = jnp.matmul(jnp.append(vb0,0), qr_linpos[b0id])
+
+            pb1 = j['pos_body1']
+            vb1 = j['vec_body1']
+
+            b1id = j['body_id1']
+            if b1id != -1:
+                # transform point on body to point in world
+                pb1 = jnp.matmul(jnp.append(pb1,1), qr_linpos[b1id])
+                vb1 = jnp.matmul(jnp.append(vb1,0), qr_linpos[b1id])
+
+            d = pb1 - pb0
+            dist_squared = jnp.sum(d*d)
+            joint_stiffness = 300000.0 
+
+            align = 1.0-jnp.sum(vb0*vb1)
+            align_stiffness = 500.0
+
+            joint_energy += 0.5 * joint_stiffness * dist_squared + 0.5 * align_stiffness * align
+
+        ###########
+
+        contact_energy = 0.0
+        ext_force_energy = 0.0
+
+        external_forces = system_def['external_forces']
+        forcedBodyId = 23 // 2
+
+        # Gravity
+        
+        massR = system_def['mass'].reshape(-1,4,4)
+
+        gravity = system_def["gravity"]
+        c_weighted = massR[:,3,3][:,None]*qr_linpos
+
+        gravity_energy = -jnp.sum(c_weighted * gravity[None,:])
+
+        return joint_energy + gravity_energy
     
     def real_kineticenergy(self, system_def, q):
-        qR = q.reshape(-1,2,1)
-        q_vel = jnp.take(qR, jnp.array([1]))
-        massR = system_def['mass']
-        lengthR = system_def['length']
-
-        inertia = massR * jnp.square(lengthR)
-        ke = 1/2*inertia*jnp.square(q_vel)
+        qRFull = jnp.concatenate((system_def['fixed_pos'],q)).reshape(-1,12,1)
+        qr_vel = jnp.array([qRFull[:,1,1], qRFull[:,3,1], qRFull[:,5,1]])
+        
+        massR = system_def['mass'].reshape(-1,4,4)
+        
+        ke = 1/2*massR[:,3,3][:,None]*jnp.square(qr_vel)
 
         return ke
     
     def dissipation_fnc(self, n, c, q):
-        qR = q.reshape(-1,2,1)
-        q_vel = jnp.take(qR, jnp.array([0]))
-        D = 1/(n+1)*c*q_vel
+        qRFull = q.reshape(-1,12,1)
+        qr_vel = jnp.array([qRFull[:,1,1], qRFull[:,3,1], qRFull[:,5,1]])
+        
+        D = 1/(n+1)*c*qr_vel
         return D
 
     def action(self, system_def, q):
@@ -180,7 +243,7 @@ class rigid2d:
         return PE+KE+dis_extforce
 
     def kinetic_energy(self, system_def, q_dot):
-        q_dotR = q_dot.reshape(-1,2,1)
+        q_dotR = q_dot.reshape(-1,12,1)
         q_pos = jnp.take(q_dotR, jnp.array([0]))
         q_vel = jnp.take(q_dotR, jnp.array([1]))
         error_arr = jnp.array([jnp.square(q_pos), jnp.square(q_vel)])
@@ -216,8 +279,23 @@ class rigid2d:
 
     def visualize(self, system_def, x, name="rigid2d", prefix='', transparency=1.):
 
-        print(x)
+        xr = jnp.concatenate((system_def['fixed_pos'],x)).reshape(-1,4,3)
+
+        for bid in range(self.n_bodies):
+            v = np.array(jnp.matmul(self.bodiesRen[bid]['W'], xr[bid]))
+            f = np.array(self.bodiesRen[bid]['f'])
+
+            ps_body = ps.register_surface_mesh("body" + prefix + str(bid), v, f)
+            if transparency < 1.:
+                ps_body.set_transparency(transparency)
+
+            transform = np.identity(4)
+            ps_body.set_transform( transform )
         
+        return ps_body # not clear that anything needs to be returned
+
+    def export(self, system_def, x, prefix=""):
+        pass        
 
     def visualize_set_nice_view(system_def, q):
         # Set a Polyscope camera view which nicely looks at the scene
